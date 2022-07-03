e46495dc37a0abf7ebd994273f4afd8f50d74bd1
Olimar Ascheri <olimar-ascheri@hotmail.com>
2022-07-03 19:58:35 -0300

    Set up

---
+++ js/main.js
@@ -0,0 +1,38 @@
+/*num1 = parseInt (prompt ('Dime un numero'))
+operador = prompt ('Ahora dime un operador')
+num2 = parseInt (prompt ('Otro numero'))
+
+let resultado = 0
+
+function sumar (num1, num2) {
+    return num1 + num2 
+}
+ 
+function restar (num1, num2) {
+    return num1 - num2 
+}
+
+function dividir (num1, num2) {
+    return num1 / num2 
+}
+
+function multiplicar (num1, num2) {
+    return num1 * num2 
+}
+
+switch (operador) {
+    case '+':
+        resultado = sumar (num1, num2)
+        break
+    case '-':
+        resultado = restar (num1, num2)
+        break    
+    case '/':
+        resultado = dividir (num1, num2)
+        break
+    case '*':
+        resultado = multiplicar (num1, num2)
+        break
+}
+
+alert(resultado)*/

d4e656b17f66c5da5543eaa8a35163a422a64355
Olimar Ascheri <olimar-ascheri@hotmail.com>
2022-07-28 23:22:45 -0300

    Carrito de compras

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 // BUSCADOR
 let carritoDeCompras = []
 
+if(localStorage.getItem("Carrito de compras") != null){
+    carritoDeCompras = JSON.parse(localStorage.getItem("Carrito de compras"))
+}
+
 let contenedorGalletas = document.getElementById ('contenedor-productos-galletas')
 let contenedorCaramelos = document.getElementById ('contenedor-productos-caramelos')
 let contenedorChocolates = document.getElementById ('contenedor-productos-chocolates')
 let contenedorPremezclas = document.getElementById ('contenedor-productos-premezclas')
 let contenedorJugos = document.getElementById ('contenedor-productos-jugos')
+
+let carroDiv = document.getElementById ('carro-div')
 
 let stockGalletas = stock.filter (galletas => galletas.tipo === "galletas")
 let stockCaramelos = stock.filter (caramelos => caramelos.tipo === "caramelos")
@@ -46,20 +52,27 @@ let stockJugos = stock.filter (jugos => jugos.tipo === "jugos")
 // Carrito
 
  const agregarCarrito = (item) => {
-
-    
-
     carritoDeCompras.push(stock[item])
-
     console.log(carritoDeCompras)
-
-   
-    
     const enJSON = JSON.stringify(carritoDeCompras)
     guardarLocal("Carrito de compras", enJSON)
 
-  
-    
+    carritoDeCompras.forEach((item) => {
+        let div = document.createElement('div')
+        div.className = 'producto'
+        div.innerHTML = `<div class="Cart-Items">
+                            <div class="about">
+                                <h1 class="title">${item.nombre}<h1>
+                                <h3 class="subtitle">250ml</h3>
+                                <img src="${item.img}" style={{ height="30px" }}/>
+                            </div>
+                            <div class="counter"></div>
+                            <div class="prices"></div>
+                        </div>
+                        `
+            carroDiv.appendChild(div) 
+
+        })
 }
 
 // Local Storage
@@ -67,3 +80,5 @@ let stockJugos = stock.filter (jugos => jugos.tipo === "jugos")
 const guardarLocal = (clave, valor) => {
    localStorage.setItem(clave, valor)
 }
+
+// Carrito de compras
